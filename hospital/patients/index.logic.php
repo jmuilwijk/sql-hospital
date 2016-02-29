cae4544b04f7de2b9c58c6ee3f216cdd28831541
jmuilwijk <jamy0184@gmail.com>
2016-02-29 09:26:49 +0100

    hospital update

---
+++ hospital/patients/index.logic.php
@@ -0,0 +1,11 @@
+<?php
+	
+	$db = new mysqli('localhost','root','','hospital');
+
+	$query = "SELECT patient.name AS patient_name, patient.spieces AS patient_spieces, 
+	patient.status AS patient_status, spieces.id AS spieces_id, spieces.spieces AS 
+	spieces_spieces from patient INNER JOIN spieces ON patient.spieces=spieces.id";
+	$result = $db->query($query);
+	
+	$patients = $result->fetch_all(MYSQLI_ASSOC);
+?>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+	header('Location:hospital/home/index.php');
+?>
